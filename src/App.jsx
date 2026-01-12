c2b2e2782027e4a1accb620170823e4bd5f1a6fa
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2026-01-12 05:03:51 -0700

    feat: Complete initial design set up and dynamically render colors based on classication using style and isInstrucment bool to determine color using conditional ? operator

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
+// Intro Section
 function MyButton() {
   return <button>Here's a button</button>;
 }
@@ -18,6 +20,12 @@ function AboutPage() {
   );
 }
 
+// About me Section
+const user = {
+  name: "Nick Alvarez",
+  imageUrl: "/headshot_CityStars.jpg",
+  imageSize: 90,
+};
 function DisplayAboutMe() {
   return (
     <>
@@ -32,11 +40,72 @@ function DisplayAboutMe() {
   );
 }
 
-const user = {
-  name: "Nick Alvarez",
-  imageUrl: "/headshot_CityStars.jpg",
-  imageSize: 90,
-};
+// Hobby section
+const hobbies = [
+  {
+    name: "Guitar",
+    imageUrl: "/AboutMe/Hobbies/alvarez-guitar-hobby.jpg",
+    imageSize: 90,
+    isInstrument: true,
+    id: 1,
+  },
+  {
+    name: "Coding",
+    imageUrl: "/AboutMe/Hobbies/coding-hobby.jpg",
+    imageSize: 90,
+    isInstrument: false,
+    id: 2,
+  },
+  {
+    name: "Piano",
+    imageUrl: "/AboutMe/Hobbies/piano-hobby.jpg",
+    imageSize: 90,
+    isInstrument: true,
+    id: 3,
+  },
+  {
+    name: "Reading",
+    imageUrl: "/AboutMe/Hobbies/reading-hobby.jpg",
+    imageSize: 90,
+    isInstrument: false,
+    id: 4,
+  },
+];
+function HobbyList() {
+  const listItems = hobbies.map((hobby) => (
+    <li key={hobby.id} style={{color: hobby.isInstrument ? 'white': 'cyan'}}>
+      {hobby.name}
+      <div>
+        <img
+          className="hobbies-img-styling"
+          src={hobby.imageUrl}
+          alt={"Photo of " + hobby.name}
+          style={{ width: hobby.imageSize, height: hobby.imageSize }}
+        />
+      </div>
+    </li>
+  ));
+  return <ul className="hobbies-list">{listItems}</ul>;
+}
+
+function FavoritesHobbies() {
+  return (
+    <>
+      <h1>Favorite Hobbies</h1>
+      <p>
+        While I enjoy many different activities, these are a few of my
+        favorites.
+      </p>
+      <div className="hobbies-container">
+        <HobbyList />
+      </div>
+    </>
+  );
+}
+
+
+
+// Main Webpage with components
 
 export default function App() {
   return (
@@ -50,6 +119,9 @@ export default function App() {
       </div>
       <div>
         <DisplayAboutMe />
+      </div>
+      <div>
+        <FavoritesHobbies />
       </div>
     </>
   );
